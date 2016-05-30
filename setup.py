f5359fe3119f970a67a5ba97540a7790bc9c9254
Ben Hoff <hoff.benjamin.k@gmail.com>
2016-05-29 22:13:55 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import find_packages, setup
+
+
+# directory = os.path.abspath(os.path.dirname(__file__))
+"""
+with open(os.path.join(directory, 'README.rst')) as f:
+    long_description = f.read()
+"""
+
+setup(
+    name="chatimus_speech",
+    version='0.0.1',
+    description='Python personal assistant',
+    # long_description=long_description,
+    url='https://github.com/benhoff/chatimus_speech',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent'],
+    author='Ben Hoff',
+    author_email='beohoff@gmail.com',
+    entry_points={'chatimusmaximus.gui': ['speech_button=speech_button.__init__:add_speech_button',]},
+    packages= find_packages(), # exclude=['docs', 'tests']
+)
---
+++ speech_button/__init__.py
@@ -0,0 +1,48 @@
+import asyncio
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QToolBar
+
+class _ToolBar(QToolBar):
+    def __init__(self, icon_1, icon_2, parent=None):
+        super().__init__(parent)
+
+        self._red_icon = icon_1
+        self._green_icon = icon_2
+
+        self.speak_action = self.addAction(self._red_icon, 'speak')
+        self.speak_action.triggered.connect(self._change_icon)
+
+    async def _change_to_red(self):
+        await asyncio.sleep(3)
+        self.speak_action.setIcon(self._red_icon)
+
+    @QtCore.pyqtSlot()
+    def _change_icon(self):
+        self.speak_action.setIcon(self._green_icon)
+        asyncio.ensure_future(self._change_to_red())
+
+
+def _send_speech_command(messager):
+    """
+    returns a function which calls the `send_command` function
+    that is passed in
+    """
+    def inner():
+        messager.send_command('speech')
+    return inner
+
+def add_speech_button(main_window, messager):
+
+
+    status_bar = main_window.status_bar
+
+    # NOTE: in the future, we'd add our own icons
+    tool_bar = _ToolBar(status_bar._red_icon,
+                        status_bar._green_icon,
+                        parent=main_window)
+
+    main_window.addToolBar(QtCore.Qt.LeftToolBarArea, tool_bar)
+    # FIXME
+    speech_triggered_slot = _send_speech_command(messager)
+    tool_bar.speak_action.triggered.connect(speech_triggered_slot)
